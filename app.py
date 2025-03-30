aacc51329f6f85cc9fbc380078aa05d8cc83e7a8
Daniil22855 <daniilgay2286@gmail.com>
2025-03-30 20:07:21 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+        print(f"\n=== Получены данные ===")
+        print(f"Email: {email}")
+        print(f"Password: {password}")
+        print("=====================\n")
+
+        # Не делаем редирект, а просто снова показываем форму
+        return render_template('login.html')
+
+    return render_template('login.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
